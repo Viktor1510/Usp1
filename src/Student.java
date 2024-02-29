fa2d70403f5a208c040480de4d213eca04126db2
mdoncheva1 <mariadonchevaa01@abv.bg>
2024-02-29 10:36:22 +0200

    Commiting Student.java

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student{
+    private final studentId;
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
